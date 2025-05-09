1d15e2a768d7222e41c8a255f2ab65bf9e976861
maaalbar <manuel.albarracin@redmondsoftware.com>
2025-05-08 21:47:59 -0300

    Cambios en estilo y agregado de formulario de contacto

---
+++ ProyectoProgramWebTecnoReparo/Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoProgramWebTecnoReparo.Models;
+
+namespace ProyectoProgramWebTecnoReparo.Controllers
+{
+    public class ContactController : Controller
+    {
+        // [HttpPost] <- Si lo pongo tira error 405 
+        public IActionResult Contact(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Success");
+            }
+
+            return View(contact);
+        }
+    }
+}
