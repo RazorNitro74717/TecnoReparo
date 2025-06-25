b1a8ae5e12eb6e1ad1086c795ac601633386e543
maaalbar <manuel.albarracin@redmondsoftware.com>
2025-06-24 22:11:45 -0300

    Agregado view para buscar mediante filtros

--- ProyectoProgramWebTecnoReparo/Controllers/ListController.cs
+++ ProyectoProgramWebTecnoReparo/Controllers/ListController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProyectoProgramWebTecnoReparo.Models;
+using ProyectoProgramWebTecnoReparo.Models.VMs;
 
 namespace ProyectoProgramWebTecnoReparo.Controllers
 {
@@ -14,12 +15,34 @@ namespace ProyectoProgramWebTecnoReparo.Controllers
         }
 
         [HttpGet]
-        public IActionResult List(int Id)
+        public IActionResult List()
         {
-            List<Product> pList = _DbContext.Products.ToList();
+            ListCatVM vm = new ListCatVM()
+            {
+                Products = _DbContext.Products.ToList(),
+                Categories = _DbContext.Categories.ToList(),
+            };
 
-            return View(pList);
+            return View(vm);
 
+        }
+
+        [HttpGet]
+        public IActionResult SearchList(string cat)
+        {
+            var prods = from pro in _DbContext.Products select pro;
+
+            if (!string.IsNullOrEmpty(cat))
+            {
+                prods = prods.Where(res => res._Category.Name.Equals(cat));
+            }
+
+            ListCatVM vm = new ListCatVM()
+            {
+                Products = prods.ToList(),
+                Categories = _DbContext.Categories.ToList(),
+            };
+            return View(vm);
         }
     }
 }
---
+++ ProyectoProgramWebTecnoReparo/Models/VMs/ListCatVM.cs
@@ -0,0 +1,8 @@
+﻿namespace ProyectoProgramWebTecnoReparo.Models.VMs
+{
+    public class ListCatVM
+    {
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; set;}
+    }
+}
